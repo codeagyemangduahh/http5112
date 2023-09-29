f86c23685a00de5f352bc4b47715165b941fcb9d
Achiaah Agyemang Duah <agyemangduahh@gmail.com>
2023-09-28 23:51:43 -0400

    Assignment 2

---
+++ N01595910Http5112Assignment2/N01595910Http5112Assignment2/Controllers/BarleyController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace N01595910Http5112Assignment2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BarleyController : ControllerBase
+    {
+        /// <summary>
+        /// Calculates the treat score of Barley the dog and returns a value indicating whether barley is happy or sad
+        /// </summary>
+        /// <param name="s"></param>
+        /// <param name="m"></param>
+        /// <param name="l"></param>
+        /// <returns>returns happy is treat score is 10 or more. Else, returns sad</returns>
+        [HttpGet("treats/small/{s}/medium/{m}/large/{l}")]
+        public IActionResult CalculateDogTreat(int s, int m, int l)
+        {
+            int treatScore = 1 * s + 2 * m + 3 * l;
+
+            return Ok(treatScore >= 10 ? "happy" : "sad");
+        }
+    }
+}
